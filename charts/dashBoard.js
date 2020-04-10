4ca41af565374345ad3afd4390bd68b262410c1b
mbarnes <mbarnes@newyu.com>
2020-04-09 22:11:51 -0700

    update dashboard with new embended Link info for updates

--- charts/dashBoard.js
+++ charts/dashBoard.js
@@ -1,17 +1,17 @@
 function createDashboard() {
-  var divElement = document.getElementById("viz1579484812567");
-  var vizElement = divElement.getElementsByTagName("object")[0];
+  var divElement = document.getElementById('viz1586409218324');
+  var vizElement = divElement.getElementsByTagName('object')[0];
   if (divElement.offsetWidth > 800) {
-    vizElement.style.width = "100%";
-    vizElement.style.height = divElement.offsetWidth * 0.75 + "px";
+    vizElement.style.width = '1420px';
+    vizElement.style.height = '2787px';
   } else if (divElement.offsetWidth > 500) {
-    vizElement.style.width = "100%";
-    vizElement.style.height = divElement.offsetWidth * 0.75 + "px";
+    vizElement.style.width = '1420px';
+    vizElement.style.height = '2787px';
   } else {
-    vizElement.style.width = "100%";
-    vizElement.style.height = "1777px";
+    vizElement.style.width = '100%';
+    vizElement.style.height = '1777px';
   }
-  var scriptElement = document.createElement("script");
-  scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
+  var scriptElement = document.createElement('script');
+  scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
   vizElement.parentNode.insertBefore(scriptElement, vizElement);
 }
